d22844a2f75836f098c78982b2774dbd31a06e7c
Kartikey Namdev <kartikeynamdev2003@gmail.com>
2025-12-25 13:09:48 +0530

    Bug fixed

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-// const DATABASE_URL = "https://backend.kartikeynamdev2003.workers.dev";
-const DATABASE_URL = "http://localhost:8787";
+const DATABASE_URL = "https://backend.kartikeynamdev2003.workers.dev";
+// const DATABASE_URL = "http://localhost:8787";
 export default DATABASE_URL;
